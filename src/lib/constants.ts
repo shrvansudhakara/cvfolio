15562238f78fc671e684932674d6a39afc4066ce
Javier Diaz <coderdiaz@gmail.com>
2025-10-29 20:24:31 -0600

    fix: updated avatar image

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,6 +1,6 @@
 import type { z } from 'astro/zod';
 import MetaDefaultImage from '@/assets/images/meta-default.jpg';
-import avatar from '@/assets/images/avatar.jpeg';
+import avatar from '@/assets/images/avatar.png';
 import type { seoSchemaWithoutImage } from '@/content.config';
 import astroConfig from 'astro.config.mjs';
 
@@ -11,17 +11,17 @@ export type AuthorInfo = {
   username?: string;
   location?: string;
   pronouns?: string;
-}
+};
 
 export type Seo = z.infer<typeof seoSchemaWithoutImage> & {
   image?: any;
-}
+};
 
 type DefaultConfigurationType = {
-  baseUrl: string,
+  baseUrl: string;
   author: AuthorInfo;
   seo: Seo;
-}
+};
 
 export const DEFAULT_CONFIGURATION: DefaultConfigurationType = {
   baseUrl: astroConfig.site || 'https://getcvfolio.com',
@@ -35,12 +35,13 @@ export const DEFAULT_CONFIGURATION: DefaultConfigurationType = {
   },
   seo: {
     title: 'CV Folio — An Astro template inspired on Read.cv',
-    description: 'Clean and aesthetic portfolio website for developers and designers',
+    description:
+      'Clean and aesthetic portfolio website for developers and designers',
     type: 'website',
     image: MetaDefaultImage,
     twitter: {
-      creator: '@cvfolio'
+      creator: '@cvfolio',
     },
     robots: 'noindex, nofollow',
-  }
+  },
 };
